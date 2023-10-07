a2a40758c62abdffcc0514b664266a3ecbe78675
PeiCIh Yang <209410181@o365.tku.edu.tw>
2023-10-07 23:10:30 +0800

    W04-P3: Using Supabase SQL editor to create table card_xx, and insert two data

--- w04_81_blog/database.js
+++ w04_81_blog/database.js
@@ -1,18 +1,24 @@
 const { Pool } = require('pg');
-
 let pool;
-
-
-
 
 pool = new Pool({
     host: 'localhost',
-    port: '5433',
+    port: '5432',
     user: 'postgres',
     password: '0000',
     database: 'wp1_demo_81',
-})
+});
 
-console.log('connect database', pool.options)
+const testDB = async () => {
+    try {
+        const results = await pool.query(`select *from card_81`);
+        console.log('json data', results.rows);
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+testDB()
+//console.log('connect database', pool.options)
 
 module.exports = pool;
