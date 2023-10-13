8b18d15858f92817731d12b50795ca8ab1983572
PeiCIh Yang <209410181@o365.tku.edu.tw>
2023-10-14 04:47:18 +0800

    W05-P3: Using database.js to connect to Supabase and show card_81 data

--- w05_81_blog/routes/api/apiCardRouter_81.js
+++ w05_81_blog/routes/api/apiCardRouter_81.js
@@ -4,12 +4,13 @@ var router = express.Router();
 import db from '../../database.js';
 
 router.get('/', async (req, res, next) => {
-    try(
-        const results = await db.query(`select = from card_81`);
-    console.log('json data', JSON.stringify(results.rows));
-    res.json(results.rows);
-    )catch (error) {
-    console.log(error);
-}
+    try {
+        results = await db.query(`select = from card_81`);
+        console.log('json data', JSON.stringify(results.rows));
+        res.json(results.rows);
+    } catch (error) {
+        console.log(error);
+    }
 });
+
 export default router;
--- w05_81_blog/server.js
+++ w05_81_blog/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import logger from 'morgan';
 import path from 'path';
-
+import apiCardRouter_81 from './routes/api/apiCardRouter_81.js';
 const app = express();
 import dotnev from 'dotenv';
 dotnev.config();
+
+import db from './database.js';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,15 +14,16 @@ app.use(logger('dev'));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use('/', (req, res, next) => {
-    res.render('index', { tittle: 'Express' });
+   res.render('index', { tittle: 'Express' });
 });
+app.use('/routes/api', apiCardRouter_81);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     if (process.env.DATABASE === 'SUPABASE') {
-        console.log('Connecting SUPABASE postgreSQL');
+        console.log(`Connecting SUPABASE postgreSQL ${db.options.database}`);
     } else {
-        console.log(`connecting local PostgreSQL`);
+        console.log(`connecting local PostgreSQL${db.options.database}`);
     }
     console.log(`Connecting on port:${port}`);
 });
