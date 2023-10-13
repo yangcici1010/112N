b1764481302ae88c5ba103f3edfc87e76afec8b1
PeiCIh Yang <209410181@o365.tku.edu.tw>
2023-10-14 02:42:52 +0800

    W05-P1: Demo how to use environment variable DATABASE

---
+++ w05_81_blog2/database.js
@@ -0,0 +1,29 @@
+const { Pool } = require('pg');
+let pool;
+
+pool = new Pool({
+    host: 'localhost',
+    port: '5432',
+    user: 'postgres',
+    password: '0000',
+    database: 'wp1_demo_81',
+});
+
+const testDB = async () => {
+    try {
+        const results = await pool.query(`select *from card_81`);
+        console.log('json data', results.rows);
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+testDB()
+//console.log('connect database', pool.options)
+app.use('/', (req, res, next) => {
+    res.render('index', { title: 'Express' });
+});
+const port = process.env.PORT || 5000;
+app.li
+module.exports = pool;
+
