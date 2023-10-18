6a3eda41ff7636abeab86a3c2ea7043873d6b4dd
PeiCIh Yang <209410181@o365.tku.edu.tw>
2023-10-18 18:35:41 +0800

    W06-P1: Run the server in W5 using /api/card_81

---
+++ w06_blog_fullstack_81/server_/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import logger from 'morgan';
+import path from 'path';
+import apiCardRouter_81 from './routes/api/apiCardRouter_81.js';
+const app = express();
+import dotnev from 'dotenv';
+dotnev.config();
+
+import db from './database.js';
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger('dev'));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+//app.use('/', (req, res, next) => {
+//    res.render('index', { tittle: 'Express' });
+//});
+app.use('/api/card_81', apiCardRouter_81);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    if (process.env.DATABASE === 'SUPABASE') {
+        console.log(`Connecting SUPABASE postgreSQL database ${db.options.database}`);
+    } else {
+        console.log(`connecting local PostgreSQL${db.options.database}`);
+    }
+    console.log(`Connecting on port:${port}`);
+});
